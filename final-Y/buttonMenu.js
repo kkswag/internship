af8abc7dcf4e091dd7d0040a4a41562de60737f1
beeativekik <kik.p@beeative.com>
2022-04-18 17:56:13 +0700

    tedy

--- final-Y/buttonMenu.js
+++ final-Y/buttonMenu.js
@@ -1,11 +1,21 @@
+var aaa = $(".menu").css("display");
+
+
 $("button").click(function () {
   var val = $(".submenu").css("display");
 
   if (val == "none") {
     $(".submenu").css("display", "block");
   } else if (val == "block") {
-    $(".submenu").css("display", "none");
+
+      $(".submenu").css("display", "none");
+
   }
 });
 
-// $("p").css("color", "blue");
+if(aaa == 'none'){
+  console.log('sss');
+  $(".submenu").css("display", "block!important");
+}
+
+
