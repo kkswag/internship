87d0e203a7806ec203efbb33f31f3be248028c64
kkswag <kamg_kob@hotmail.co.th>
2022-04-18 18:03:49 +0700

    up

--- final-Y/buttonMenu.js
+++ final-Y/buttonMenu.js
@@ -1,21 +1,18 @@
-var aaa = $(".menu").css("display");
-
-
-$("button").click(function () {
-  var val = $(".submenu").css("display");
-
-  if (val == "none") {
-    $(".submenu").css("display", "block");
-  } else if (val == "block") {
-
-      $(".submenu").css("display", "none");
-
+$(“button”).click(function () {
+  var val = $(“.submenu”).css(“display”);
+  if (val == “none”) {
+    $(“.submenu”).css(“display”, “block”);
+  } else if (val == “block”) {
+    $(“.submenu”).css(“display”, “none”);
   }
 });
-
-if(aaa == 'none'){
-  console.log('sss');
-  $(".submenu").css("display", "block!important");
-}
-
-
+// if (“.menu” == “none”) {
+//   $(“.submenu”).css(“display”, “block”);
+// } else if ((“.menu” == “display”, “block”)) {
+//   $(“.submenu”).css(“display”, “.none”);
+// }
+// else if (‘.menu’ == ‘display’,‘block’)
+// {
+//     $(‘.’)
+// }
+// $(“p”).css(“color”, “blue”);
