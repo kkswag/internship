c1da3ed940fbbb6558363ec283576539742b7804
beeativekik <kik.p@beeative.com>
2022-04-19 11:57:07 +0700

    df

--- final-Y/buttonMenu.js
+++ final-Y/buttonMenu.js
@@ -1,18 +1,18 @@
-$(“button”).click(function () {
-  var val = $(“.submenu”).css(“display”);
-  if (val == “none”) {
-    $(“.submenu”).css(“display”, “block”);
-  } else if (val == “block”) {
-    $(“.submenu”).css(“display”, “none”);
+$('button').click(function () {
+  var val = $('.submenu').css('display');
+  if (val == 'none') {
+    $('.submenu').css('display', 'block');
+  } else if (val == 'block') {
+    $('.submenu').css('display', 'none');
   }
 });
-// if (“.menu” == “none”) {
-//   $(“.submenu”).css(“display”, “block”);
-// } else if ((“.menu” == “display”, “block”)) {
-//   $(“.submenu”).css(“display”, “.none”);
+// if ('.menu' == 'none') {
+//   $('.submenu').css('display', 'block');
+// } else if (('.menu' == 'display', 'block')) {
+//   $('.submenu').css('display', '.none');
 // }
 // else if (‘.menu’ == ‘display’,‘block’)
 // {
 //     $(‘.’)
 // }
-// $(“p”).css(“color”, “blue”);
+// $('p').css('color', 'blue');
