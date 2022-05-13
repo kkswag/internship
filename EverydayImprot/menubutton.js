500a45ec741b46afde86fbe28a9b1d86b153b549
kkswag <kamg_kob@hotmail.co.th>
2022-05-13 15:41:56 +0700

    dd

--- EverydayImprot/menubutton.js
+++ EverydayImprot/menubutton.js
@@ -1,7 +1,26 @@
-$(".menubottom>button:last-child").click(function () {
-  var val = $(".menubottom").css("display");
+// $(".menuBottom>button:last-child").click(function () {
+//   var val = $(".menubottom").css("display");
 
-  $(".menubutton").toggleClass("dropdownLanguage");
+//   $(".menubutton").toggleClass("dropdownLanguage");
+
+//   console.log("a");
+// });
+
+// $(".menuBottom>button:last-child").click(function () {
+//   var val = $(".menuBottom").css("display");
+
+//   if (val == "none") {
+//     $(".menuButton").addClass("dropdownLanguage");
+//   } else if (val == "block") {
+//     $(".menuButton").removeClass("dropdownLanguage");
+//   }
+//   console.log("a");
+// });
+
+$(".menuBottom>button:last-child").hover(function () {
+  var val = $(".dropdownLanguage").css("display");
+
+  $(".dropdownLanguage").toggleClass("showLanguage");
 
   console.log("a");
 });
